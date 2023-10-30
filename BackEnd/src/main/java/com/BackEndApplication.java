4eccb68f80d4279ca1eb79c58436ddfee6ca277a
Ioannis Zoulias <zoulias@yahoo.com>
2023-10-30 09:51:12 +0200

    BE first commit

---
+++ BackEnd/src/main/java/com/BackEndApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+		System.out.println("spring boot up");
+	}
+
+}
---
+++ BackEnd/src/main/java/com/bean/Login.java
@@ -0,0 +1,71 @@
+package com.bean;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Login {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int uid;
+	private String username;
+	private String password;
+	private int isadmin;
+	
+	@OneToMany (mappedBy = "login")
+	private List<OrderInfo> listOfOrders;
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getIsadmin() {
+		return isadmin;
+	}
+
+	public void setIsadmin(int isadmin) {
+		this.isadmin = isadmin;
+	}
+
+	public List<OrderInfo> getListOfOrders() {
+		return listOfOrders;
+	}
+
+	public void addOrder (OrderInfo orderInfo) {
+		this.listOfOrders.add(orderInfo);
+		orderInfo.setLogin(this);
+	}
+
+	@Override
+	public String toString() {
+		return "Login [uid=" + uid + ", username=" + username + ", password=" + password + ", isadmin=" + isadmin
+				+ ", listOfOrders=" + listOfOrders + "]";
+	}
+
+}
